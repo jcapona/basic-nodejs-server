04e1919c8b7819c129f28077de7f27be019dcae1
Jorge Capona <jcapona@gmail.com>
2016-01-09 16:55:21 -0300

    Raw TCP timeserver

---
+++ timeServer.js
@@ -0,0 +1,30 @@
+/*
+ ## TIME SERVER (Exercise 10 of 13)  
+   
+  Write a TCP time server!  
+   
+  Your server should listen to TCP connections on the port provided by the  
+  first argument to your program. For each connection you must write the  
+  current date & 24 hour time in the format:  
+   
+     "YYYY-MM-DD hh:mm"  
+   
+  followed by a newline character. Month, day, hour and minute must be  
+  zero-filled to 2 integers. For example:  
+   
+     "2013-07-06 17:42"  
+*/
+
+var net = require('net');
+
+var port = process.argv[2];
+
+var server = net.createServer(function(socket){
+    var date = new Date();
+    var month = Number(date.getMonth())+1 < 10? "0"+(Number(date.getMonth())+1).toString() : (Number(date.getMonth())+1).toString();
+    var day = Number(date.getDate()) < 10? "0"+date.getDate() : date.getDate();
+    socket.end(date.getFullYear()+"-"+month+"-"+day+" "+date.getHours()+":"+date.getMinutes());
+    console.log(socket);
+});
+
+server.listen(port);
